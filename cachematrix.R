fcb2c4c2e8afae96c898ba6bd4a105cb1402f10b
Andrey Belyakov <andrey.belyakov@macys.com>
2017-05-17 13:22:14 +0300

    CacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for creating a matrix with cacheable "solve" function
 
-## Write a short comment describing this function
-
+## Makes an object for caching of result of solve 
+## function which contains a list of functions.
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(m) {
+    x <<- m
+    s <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) s <<- solve
+  getSolve <- function() s
+  list(set = set, 
+       get = get, 
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
-
+## Returns result of "solve" function from a cache 
+## if it had been calculated or calculates result if it hadn't been calculated.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  	slv <- x$getSolve()
+  	if(!is.null(slv)) {
+   		message("getting cached data")
+   		return(slv)
+  	}
+  	data <- x$get()
+  	slv <- solve(data, ...)
+  	x$setSolve(slv)
+  	slv
+	}
 }
